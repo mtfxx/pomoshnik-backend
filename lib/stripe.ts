a2e40aeb8b2a1a8b2238d3c5a6f60d56e85db0e2
mtfxx <mtfxx@users.noreply.github.com>
2026-02-10 14:38:41 -0500

    Fix: simplify Stripe init to resolve connection error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,5 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-01-28.clover',
-  typescript: true,
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default stripe;
